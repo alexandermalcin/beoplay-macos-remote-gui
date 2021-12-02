7b56bac3626bd49c924a01a07b291b5089cec111
amalcin <yourinput@me.com>
2021-12-02 21:40:44 +0100

    set image-info-view appearance background color s and icons

--- Sources/BeoplayRemoteGUI/InformationImageView.swift
+++ Sources/BeoplayRemoteGUI/InformationImageView.swift
@@ -6,17 +6,7 @@
 //
 
 import Foundation
-import SwiftUI
 import Cocoa
-
-extension Notification.Name {
-    static let AppleInterfaceThemeChangedNotification = Notification.Name("AppleInterfaceThemeChangedNotification")
-}
-
-enum OSAppearance: Int {
-    case light
-    case dark
-}
 
 @IBDesignable
 class InformationImageView: NSImageView {
@@ -45,51 +35,15 @@ class InformationImageView: NSImageView {
         }
     }
     
-    var osAppearance: OSAppearance = .light
-    
     required init?(coder: NSCoder) {
         super.init(coder: coder)
         wantsLayer = true
         layer?.setNeedsDisplay()
-        getAppearance()
-        listenToInterfaceChangesNotification()
     }
     
-    func listenToInterfaceChangesNotification() {
-        DistributedNotificationCenter.default.addObserver(
-            self,
-            selector: #selector(interfaceModeChanged),
-            name: .AppleInterfaceThemeChangedNotification,
-            object: nil
-        )
+    override func layout() {
+        super.layout()
+        self.layer?.backgroundColor = NSColor(named: NSColor.Name("ImageViewBackgroundColor"))?.cgColor
     }
     
-    @objc func interfaceModeChanged(_: NSNotification) {
-         getAppearance()
-    }
-    
-    func getAppearance() {
-        let appearanceDescription = NSApplication.shared.effectiveAppearance.debugDescription.lowercased()
-        self.osAppearance = appearanceDescription.contains("dark") ? .dark : .light
-        updateAppearance()
-    }
-    
-    func updateAppearance() {
-        DispatchQueue.main.async {
-            switch self.osAppearance {
-            case .light:
-                self.layer?.backgroundColor = NSColor(red: 0, green: 0, blue: 0, alpha: 0.2).cgColor
-            case .dark:
-                self.layer?.backgroundColor = NSColor(red: 1, green: 1, blue: 1, alpha: 0.2).cgColor
-            }
-        }
-    }
-    
-}
-
-struct InfoImageView_Previews: PreviewProvider {
-    @available(OSX 10.15.0, *)
-    static var previews: some View {
-        Text("")
-    }
 }
